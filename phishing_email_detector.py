28fb7a86423f99efa965b1bf26e0bdd2bd917815
GinaJPS <reginajps@gmail.com>
2026-02-18 22:28:49 +0000

    Completed the phishing_email_detector project

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -1,8 +1,8 @@
 # FILE NAME - phishing_email_detector.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Regina Swartout
+# DATE: 2/18/26
+# BRIEF DESCRIPTION:  A program that analyzes an email subject line to detect phishing attempts based on common red flags.
 
 
 
@@ -14,14 +14,22 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
+subject = input("Enter the email subject line: ")
+print()
+print("SECURITY ASSESSMENT:")
+
+if "urgent" in subject.lower() or "immediate action required" in subject.lower():
+    print("HIGH RISK: Possible phishing attempt.")
+elif "win" in subject.lower() or "free" in subject.lower():
+    print("MEDIUM RISK: Suspicious offer detected.")
+elif "password reset" in subject.lower() or "free" in subject.lower():
+    print("LOW RISK: Verify legitimacy with sender.")
+else:
+    print("No phishing indicators detected.")
 
 
-
-
-
-
-
-
+print("------------------------")
+print(f'Analyzed subject: "{subject}"')
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
@@ -77,11 +85,10 @@ Analyzed subject: "Did you request a password reset?"
 '''
 
 1. Was using `in` difficult or was it natural?
-
-
-
-
-
+It felt natural to use the keyword 'in' in this program, especially
+because I have experience with Python already.  I also used 'or' but I can't
+remember if we saw it in the notes.  I hope that is ok.  I will change it if 
+it looks like the grading system doesn't like that I used 'or' yet.
 
 
 '''
@@ -97,7 +104,7 @@ Analyzed subject: "Did you request a password reset?"
 Please gauge your utilization of AI on the following spectrum. Place an "X" in front
 of the appropriate response. Only choose one of the following:
 
-[ ] I did not use AI at all for this lab.
+[X] I did not use AI at all for this lab.
 [ ] I wrote the initial draft of the software but had AI help me make it better.
 [ ] I fed the lab description to AI and had it generate a response but I modified it.
 [ ] AI created the entire program for me.
@@ -112,6 +119,6 @@ may impede your understanding. Please rate how well you understand the concepts 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+[X] I'm solid. Totally got it.
 
 '''
